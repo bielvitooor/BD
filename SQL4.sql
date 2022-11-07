4623e12dc4aab96a74ec2930a8ae814b7c7cc41f
Isaque Pontes Romualdo <isaquepontes47@gmail.com>
2022-11-07 19:50:06 -0300

    Update SQL4.sql

--- SQL4.sql
+++ SQL4.sql
@@ -106,4 +106,7 @@ CREATE TABLE Setor_has_Registros(
    INSERT INTO adrianob_grupo09.Sexo(id_sexo,descricao_sexo) values(2,"Feminino");
     Select * from Pessoas;    
     INSERT INTO adrianob_grupo09.Pessoas(nome,matricula,cpf,email,telefone,dt_nasc,sexo_id_sexo) values("Gabriel",20203930249303, 11109046781,"gabriel@gmail.com","(62)98607-3404","2004-03-19",1);
-    alter table Pessoas;
+    INSERT INTO adrianob_grupo09.Pessoas(nome,matricula,cpf,email,telefone,dt_nasc,sexo_id_sexo) values("Vanessa",20203930249373, 11104046781,"vanessa@gmail.com","(62)98607-3404","2004-03-19", 2);
+    INSERT INTO adrianob_grupo09.Pessoas(nome,matricula,cpf,email,telefone,dt_nasc,sexo_id_sexo) values("Bruna",20203930455403, 45512335565,"bruna@gmail.com","(62)99856-9858","2003-01-28",2);
+    INSERT INTO adrianob_grupo09.Pessoas(nome,matricula,cpf,email,telefone,dt_nasc,sexo_id_sexo) values("Isaque",20203930455403, 11254785458,"isaque@gmail.com","(62)99856-9858","2004-03-17",1);
+    
