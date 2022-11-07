e8cc46f712a8829b5184c2fe632c44f9e6545907
Isaque Pontes Romualdo <isaquepontes47@gmail.com>
2022-11-07 20:21:07 -0300

    Create pessoas.sql

---
+++ Pessoas/pessoas.sql
@@ -0,0 +1,6 @@
+SELECT * FROM adrianob_grupo09.Pessoas;
+Select * from Pessoas;    
+    INSERT INTO adrianob_grupo09.Pessoas(nome,matricula,cpf,email,telefone,dt_nasc,sexo_id_sexo) values("Gabriel",20203930249303, 11109046781,"gabriel@gmail.com","(62)98607-3404","2004-03-19",1);
+    INSERT INTO adrianob_grupo09.Pessoas(nome,matricula,cpf,email,telefone,dt_nasc,sexo_id_sexo) values("Vanessa",20203930249373, 11104046781,"vanessa@gmail.com","(62)98607-3404","2004-03-19", 2);
+    INSERT INTO adrianob_grupo09.Pessoas(nome,matricula,cpf,email,telefone,dt_nasc,sexo_id_sexo) values("Bruna",20203930455403, 45512335565,"bruna@gmail.com","(62)99856-9858","2003-01-28",2);
+    INSERT INTO adrianob_grupo09.Pessoas(nome,matricula,cpf,email,telefone,dt_nasc,sexo_id_sexo) values("Isaque",20203930455403, 11254785458,"isaque@gmail.com","(62)99856-9858","2004-03-17",1);
